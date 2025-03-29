ddeeb7578ba28759fa4dbe28b4d338dab8b65d28
ImAadarsh <aadarshkavita@gmail.com>
2025-03-29 18:16:26 +0530

    AA

--- inner_pages/faqs.php
+++ inner_pages/faqs.php
@@ -74,46 +74,7 @@
 
     <!-- ==================== Start Navbar ==================== -->
 
-    <nav class="navbar navbar-expand-lg">
-        <div class="container">
-
-            <!-- Logo -->
-            <a class="logo" href="#">
-                <img src="assets/imgs/Logo-light.svg" alt="logo">
-            </a>
-
-            <!-- navbar links -->
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="../inner_pages/about.html">Studio</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="../inner_pages/portfolio-cards.html">Works</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="../inner_pages/blog-grid.html">News</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="topnav d-flex align-items-center">
-                <a href="../inner_pages/contact.php" class="butn butn-rounded">
-                    <div class="d-flex align-items-center">
-                        <span>Start Project</span>
-                        <span class="icon ml-10">
-                            <img src="../common/imgs/icons/arrow-top-right.svg" alt="">
-                        </span>
-                    </div>
-                </a>
-                <div class="menu-icon cursor-pointer">
-                    <span class="icon ti-align-right"></span>
-                </div>
-            </div>
-        </div>
-    </nav>
+<?php include 'components/header.php'; ?>
 
     <div class="hamenu">
         <div class="close-menu cursor-pointer ti-close"></div>
--- inner_pages/services.php
+++ inner_pages/services.php
@@ -74,46 +74,7 @@
 
     <!-- ==================== Start Navbar ==================== -->
 
-    <nav class="navbar navbar-expand-lg">
-        <div class="container">
-
-            <!-- Logo -->
-            <a class="logo" href="#">
-                <img src="assets/imgs/Logo-light.svg" alt="Connect Everest">
-            </a>
-
-            <!-- navbar links -->
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="#">About Us</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Employer</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Employee</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Students</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="topnav d-flex align-items-center">
-                <a href="#" class="butn butn-rounded">
-                    <div class="d-flex align-items-center">
-                        <span>Contact Us</span>
-                        <span class="icon ml-10">
-                            <img src="../common/imgs/icons/arrow-top-right.svg" alt="">
-                        </span>
-                    </div>
-                </a>
-                <div class="menu-icon cursor-pointer">
-                    <span class="icon ti-align-right"></span>
-                </div>
-            </div>
-        </div>
-    </nav>
+<?php include 'components/header.php'; ?>
 
     <div class="hamenu">
         <div class="close-menu cursor-pointer ti-close"></div>
