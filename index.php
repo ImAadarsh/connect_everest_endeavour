d432c90c5ca8a2491a0f048e536c3115d1bdaed9
ImAadarsh <aadarshkavita@gmail.com>
2025-03-29 18:12:21 +0530

    AA

--- index.php
+++ index.php
@@ -73,7 +73,49 @@
 
     <!-- ==================== Start Navbar ==================== -->
 
-<?php include 'inner_pages/components/header.php'; ?>
+    <nav class="navbar navbar-expand-lg">
+        <div class="container">
+
+            <!-- Logo -->
+            <a class="logo" href="index.php">
+                <img src="assets/imgs/Logo-light.svg" alt="Connect Everest">
+            </a>
+
+            <!-- navbar links -->
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="inner_pages/about.php">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="inner_pages/employer.php">Employer</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="inner_pages/employee.php">Employee</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="inner_pages/student.php">Students</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="inner_pages/services.php">Services</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="topnav d-flex align-items-center">
+                <a href="inner_pages/contact.php" class="butn butn-rounded">
+                    <div class="d-flex align-items-center">
+                        <span>Contact Us</span>
+                        <span class="icon ml-10">
+                            <img src="../common/imgs/icons/arrow-top-right.svg" alt="">
+                        </span>
+                    </div>
+                </a>
+                <div class="menu-icon cursor-pointer">
+                    <span class="icon ti-align-right"></span>
+                </div>
+            </div>
+        </div>
+    </nav>
 
     <div class="hamenu">
         <div class="close-menu cursor-pointer ti-close"></div>
